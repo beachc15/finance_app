9bb6f3d95c5a9f90ec6c5157cb32af5cfd02826a
beachc15 <60740158+beachc15@users.noreply.github.com>
2020-04-05 19:56:01 -0700

    updated for addition of monthly prices and statistics

--- varcovar/views.py
+++ varcovar/views.py
@@ -4,6 +4,7 @@ import json
 import requests
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from datetime import datetime
 
 from .forms import NameForm
 
@@ -15,10 +16,8 @@ def data_view(request):
         tickers = request.POST['tickers']
         frequency = request.POST['frequency']
         period = request.POST['period']
+        tickers = tickers.split(', ')
 
-
-        print(frequency)
-        print(period)
 
         remote_url = 'https://sleepy-garden-10843.herokuapp.com/api'
 
@@ -26,10 +25,40 @@ def data_view(request):
         headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
         r = requests.post(remote_url, data=j_data, headers=headers)
         z = r.json()
-        df = pd.DataFrame(z['data'])
+        # print(z['matrix'])
+        data = json.loads(z['matrix'])['data']
+        stats = z['statistics']
+        prices = stats['month_stats']['stat_prices']
+        prices = pd.DataFrame(json.loads(prices))
+        dates = list(map(int, prices.index))
+        formatted_dates = []
+        for date in dates:
+            formatted_dates.append(datetime.utcfromtimestamp(float(date)/1000.).date())
+        prices.insert(0, 'Dates', formatted_dates)
+        df = pd.DataFrame(data)
         df = df.round(5)
-        return render(request, 'varcovar/api.html', {'DataFrame': df})
-    except:
+
+        yearly_stats = stats['year_stats']
+        year_series = pd.DataFrame({'average': json.loads(yearly_stats['average']), 'variance': json.loads(yearly_stats['variance']), 'Standard Deviation': json.loads(yearly_stats['simga'])})
+        year_series = year_series.transpose()
+        year_series.insert(0, 'index', ['Average', 'Variance', 'Std-dev'])
+
+        monthly_stats = stats['month_stats']
+        month_series = pd.DataFrame(
+            {'average': json.loads(monthly_stats['average']), 'variance': json.loads(monthly_stats['variance']),
+             'Standard Deviation': json.loads(monthly_stats['sigma'])})
+        month_series = month_series.transpose()
+        month_series.insert(0, 'index', ['Average', 'Variance', 'Std-dev'])
+
+        gen_stats = json.loads(stats['stats'])['data']
+        gen_stats = pd.DataFrame(gen_stats)
+        for stat in json.loads(stats['stats']):
+            print(stat)
+
+
+
+        return render(request, 'varcovar/api.html', {'DataFrame': df, 'prices': prices, 'year_stats': year_series, 'month_stats': month_series, 'gen_stats': gen_stats})
+    except ModuleNotFoundError:
         return render(request, 'varcovar/index.html', {'form': NameForm})
 
 
